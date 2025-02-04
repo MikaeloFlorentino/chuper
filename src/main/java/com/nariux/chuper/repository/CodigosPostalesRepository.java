fba31beced47395905b77cdfec69f2f04471d36f
mikaelo <miguel.hdz.mrn@gmail.com>
2025-02-04 11:55:51 -0600

    Correccon de paremetro en el JPA

--- src/main/java/com/nariux/chuper/repository/CodigosPostalesRepository.java
+++ src/main/java/com/nariux/chuper/repository/CodigosPostalesRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nariux.chuper.entity.CodigoPostal;
 
-public interface CodigosPostalesRepository  extends JpaRepository<CodigoPostal, Integer>{
-
+public interface CodigosPostalesRepository extends JpaRepository<CodigoPostal, String>{
+    
 }
